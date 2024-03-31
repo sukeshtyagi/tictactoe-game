f6545108b8f03b87044d42d8a339fdd8778942e1
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-03-31 09:04:51 +0530

    Landscape and portrait

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -13,7 +13,7 @@ function DetailsPage() {
 
   return (
     <div className="outer box-border w-screen h-screen bg-black pt-2">
-      <div className="details box-border w-3/4 sm:w-1/2 h-4/6 bg-white mx-auto mt-32 p-4 rounded-xl flex flex-col items-start justify-evenly">
+      <div className="details box-border w-3/4 sm:w-1/2 portrait:h-fit bg-white mx-auto mt-10 landscape:mt-0 landscape:h-10/12 p-4 rounded-xl flex flex-col items-start justify-evenly gap-10">
         <div className="playerOne box-border w-full h-2/6 bg-green-400 flex flex-col items-start border rounded-xl border-black px-2 gap-2">
           <label className="mt-1">Enter Player 1 Name</label>
           <input
